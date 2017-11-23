08633be0758c2a65cbf0a7e5cc6478c5111151d0
Marc Foley <m.foley.88@gmail.com>
2017-11-23 11:41:00 +0000

    Run through shell script to stop memory leak

--- bin/test_roboto_hinted_src.py
+++ bin/test_roboto_hinted_src.py
@@ -8,9 +8,6 @@ WARNING: Run time is astronomical.
 This should only be run if the 3rd party supplier has updated
 cvt, fpgm and prep tables.
 
-To avoid source mixups, the hinted sources are downloaded from the
-github repository.
-
 Throw this onto AWS or another cloud platform.
 
 """
@@ -19,6 +16,7 @@ import argparse
 import requests
 import json
 import os
+import sys
 from ntpath import basename
 from glob import glob
 import shutil
@@ -54,63 +52,33 @@ def download_current_hinted_fonts(dst):
     return filepaths
 
 
-def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('fonts', nargs="+",
-                         help="Fonts to be tested against existing sources")
-    parser.add_argument('out_dir')
+def main(font_before, font_after, out_dir):
+    """Due to the fonts containing so many glyphs, style on style
+    comparisons are needed."""
     auth = load_browserstack_credentials()
-    args = parser.parse_args()
+    logger.info('Comparing %s' % os.path.basename(font_before))
+    fullname = font_before[:-4]
+    font_dir = os.path.join(out_dir, fullname)
+    diffbrowsers = DiffBrowsers(auth, font_dir,
+                              [font_after],
+                              [font_before],
+                              test_browsers['all_browsers'])
 
-    browsers = test_browsers['all_browsers']
-    fonts_before_dir = os.path.join(args.out_dir, 'before_fonts')
-    fonts_after_dir = os.path.join(args.out_dir, 'after_fonts')
-
-    for path in (args.out_dir, fonts_before_dir, fonts_after_dir):
-        if not os.path.isdir(path):
-            os.mkdir(path)
-
-    download_current_hinted_fonts(fonts_before_dir)
-    fonts_before = {basename(f): os.path.join(fonts_before_dir, f) for f
-                    in os.listdir(fonts_before_dir) if f.endswith('.ttf')}
-
-    for font_path in args.fonts:
-        logger.info('Copying %s to %s' % (font_path, fonts_after_dir))
-        shutil.copy(font_path, fonts_after_dir)
-
-    fonts_after = {basename(f): os.path.join(fonts_after_dir, f) for f
-                   in os.listdir(fonts_after_dir) if f.endswith('.ttf')}
-
-    shared_fonts = set(fonts_before.keys()) & set(fonts_after.keys())
-    if not shared_fonts:
-        raise Exception('Your Fonts are not Roboto. '
-                        'Filenames must match hinted sources')
-    # Due to the fonts containing so many glyphs, style on style comparisons
-    # are needed.
-    for font_path in shared_fonts:
-        logger.info('Comparing %s' % font_path)
-        fullname = font_path[:-4]
-        font_dir = os.path.join(args.out_dir, fullname)
-        diffbrowsers = DiffBrowsers(auth, font_dir,
-                                  [fonts_after[font_path]],
-                                  [fonts_before[font_path]],
-                                  browsers)
-
-        diffbrowsers.diff_view('waterfall', gen_gifs=True)
+    diffbrowsers.diff_view('waterfall', gen_gifs=True)
+    logger.info('Resting Browserstack screenshot api')
+    time.sleep(10)
+    
+    # Generate glyph plots for Material Design sp sizes
+    for n in [11, 12, 13, 14, 16, 15, 20, 24, 34, 45, 56, 112]:
+        diffbrowsers.diff_view('glyphs-all', pt=n, gen_gifs=True)
         logger.info('Resting Browserstack screenshot api')
         time.sleep(10)
-        
-        # Generate glyph plots for Material Design sp sizes
-        for n in [11, 12, 13, 14, 16, 15, 20, 24, 34, 45, 56, 112]:
-            diffbrowsers.diff_view('glyphs-all', pt=n, gen_gifs=True)
-            logger.info('Resting Browserstack screenshot api')
-            time.sleep(10)
 
-        report = cli_reporter(diffbrowsers.report)
-        report_path = os.path.join(font_dir, 'report.txt')
-        with open(report_path, 'w') as doc:
-            doc.write(report)
+    report = cli_reporter(diffbrowsers.report)
+    report_path = os.path.join(font_dir, 'report.txt')
+    with open(report_path, 'w') as doc:
+        doc.write(report)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
