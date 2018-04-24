de72dfda41eeae8df8379c6d9635dae25530d59f
Marc Foley <m.foley.88@gmail.com>
2018-04-24 12:51:00 +0100

    Load and diff pre-existing GF Regression sessions.
    In the previous implementation, users couldn't take diff images of an
    existing session. Diffbrowsers would only allow new sessions to be created by
    uploading fresh fonts each time.
    
    In order to facilitate this change. The CLI interface has been updated using
    argparse's submodules.
    
    If a user would like to diff an existing session, they add load to their
    command e.g
    
    ```
    gfdiffbrowsers load <url> -o ~/Desktop/img_dir
    ```
    
    If a user would like to create a new session, they add new to their
    command e.g
    
    ```
    gfdiffbrowsers new <url> [fonts_after] -fb [fonts_before] -o ~/Desktop/img_dir
    ```
    
    Some api changes had to be made in order to facilitate this new functionality.
    Fortunately only one tool relies on this api, the Dispatcher, which is not
    publicly available yet.

--- Lib/diffbrowsers/__main__.py
+++ Lib/diffbrowsers/__main__.py
@@ -12,10 +12,13 @@ matched by filenames.
 See README.md for further info
 
 Basic Usage:
-diffbrowsers [fonts_after] -fb [fonts_before] -o ~/Desktop/font_img_dir
+gfdiffbrowsers new [fonts_after] -fb [fonts_before] -o ~/Desktop/font_img_dir
 
 Compare against family hosted on Google Fonts:
-diffbrowsers [fonts_after] -gf -o ~/Desktop/font_img_dir
+gfdiffbrowsers new [fonts_after] -gf -o ~/Desktop/font_img_dir
+
+Load a previous session:
+gfdiffbrowsers load <url> -o ~/Desktop/font_img_dir
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
 import argparse
@@ -27,38 +30,49 @@ from diffbrowsers.utils import load_browserstack_credentials, cli_reporter
 
 
 def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    
-    parser.add_argument('fonts_after', nargs="+", help="Fonts after paths")
-    before_group = parser.add_argument_group(title="Fonts before input")
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument('-o', '--output-dir', help="Directory for output images",
+                            required=True)
+    parent_parser.add_argument('-pt', '--type-point-size',
+                            help="In some views, users can control type sample size")
+    parent_parser.add_argument('-b', '--browsers',
+                        choices=['all_browsers', 'gdi_browsers', 'android_browsers'],
+                        default='all_browsers',
+                        help="Which set of browsers to test on")
+    parent_parser.add_argument('-v', '--view', choices=[
+                        'waterfall', 'glyphs-new', 'glyphs-modified',
+                        'glyphs-missing', 'glyphs-all'], default='waterfall')
+    parent_parser.add_argument('-gif', '--output-gifs', action='store_true', default=False,
+                        help="Output before and after gifs")
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command')
+
+    upload_parser = subparsers.add_parser('new', parents=[parent_parser])
+    upload_parser.add_argument('fonts_after', nargs="+", help="Fonts after paths")
+    before_group = upload_parser.add_argument_group(title="Fonts before input")
     before_input_group = before_group.add_mutually_exclusive_group(required=True)
     before_input_group.add_argument('-fb', '--fonts-before', nargs="+",
                                   help="Fonts before paths")
     before_input_group.add_argument('-gf', '--from-googlefonts', action='store_true',
                                help="Diff against GoogleFonts instead of fonts_before")
-    parser.add_argument('-o', '--output-dir', help="Directory for output images",
-                        required=True)
-    parser.add_argument('-pt', '--type-point-size',
-                        help="In some views, users can control type sample size")
-    parser.add_argument('-b', '--browsers',
-                        choices=['all_browsers', 'gdi_browsers', 'android_browsers'],
-                        default='all_browsers',
-                        help="Which set of browsers to test on")
-    parser.add_argument('-v', '--view', choices=[
-                        'waterfall', 'glyphs-new', 'glyphs-modified',
-                        'glyphs-missing', 'glyphs-all'], default='waterfall')
-    parser.add_argument('-gif', '--output-gifs', action='store_true', default=False,
-                        help="Output before and after gifs")
+
+    load_parser = subparsers.add_parser('load', parents=[parent_parser])
+    load_parser.add_argument("url")
+
     args = parser.parse_args()
 
-    auth = load_browserstack_credentials()
     browsers_to_test = test_browsers[args.browsers]
 
-    fonts_before = 'from-googlefonts' if args.from_googlefonts \
-                    else args.fonts_before
+    diffbrowsers = DiffBrowsers(dst_dir=args.output_dir, browsers=browsers_to_test)
 
-    diffbrowsers = DiffBrowsers(auth, fonts_before, args.fonts_after,
-                                args.output_dir, browsers_to_test)
+    if args.command == 'new':
+        fonts_before = 'from-googlefonts' if args.from_googlefonts \
+                       else args.fonts_before
+        diffbrowsers.new_session(fonts_before, args.fonts_after)
+    elif args.command == 'load':
+        diffbrowsers.load_session(args.url)
+
     diffbrowsers.diff_view(args.view, args.type_point_size, args.output_gifs)
 
     report_path = os.path.join(args.output_dir, 'report.txt')
