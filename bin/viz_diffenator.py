1e07a6d460ec687dd84e51366de9330406dfb5d7
Marc Foley <m.foley.88@gmail.com>
2018-11-08 15:21:11 +0000

    viz_diffenator: diff fonts against fonts hosted on Google Fonts

--- bin/viz_diffenator.py
+++ bin/viz_diffenator.py
@@ -14,25 +14,34 @@ logger = logging.getLogger(__name__)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fonts_a', nargs="+", required=True)
-    parser.add_argument('--fonts_b', nargs="+", required=True)
+
+    parser.add_argument('fonts_after', nargs="+", help="Fonts after paths")
+    before_group = parser.add_argument_group(title="Fonts before input")
+    before_input_group = before_group.add_mutually_exclusive_group(required=True)
+    before_input_group.add_argument('-fb', '--fonts-before', nargs="+",
+                                    help="Fonts before paths")
+    before_input_group.add_argument('-gf', '--from-googlefonts', action='store_true',
+                               help="Diff against GoogleFonts instead of fonts_before")
+
+
     parser.add_argument('-u', '--gfr-url', default=GF_PRODUCTION_URL,
                         help="Url to GFR instance")
     parser.add_argument('-l', '--gfr-local', action="store_true", default=False)
+    parser.add_argument('-o', '--output-dir', help="Directory for output images",
+                               required=True)
     args = parser.parse_args()
 
     browsers_to_test = test_browsers['safari_latest']
-    output_dir = 'out'
-    if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
-    font_img_dir = os.path.join(output_dir, os.path.basename(args.fonts_a)[:-4])
 
     diffbrowsers = DiffBrowsers(gfr_instance_url=args.gfr_url,
                                 gfr_is_local=args.gfr_local,
-                                dst_dir=font_img_dir,
+                                dst_dir=args.output_dir,
                                 browsers=browsers_to_test)
 
-    diffbrowsers.new_session(args.fonts_a, args.fonts_b)
+    fonts_before = 'from-googlefonts' if args.from_googlefonts \
+                    else args.fonts_before
+
+    diffbrowsers.new_session(fonts_before, args.fonts_after)
 
     views_to_diff = diffbrowsers.gf_regression.info['diffs']
     logger.info("Following diffs have been found [%s]. Genning images." % ', '.join(views_to_diff))
@@ -43,7 +52,7 @@ def main():
         else:
             diffbrowsers.diff_view(view, pt=32)
 
-    logger.info("Images saved to {}".format(output_dir))
+    logger.info("Images saved to {}".format(args.output_dir))
 
 
 if __name__ == '__main__':
